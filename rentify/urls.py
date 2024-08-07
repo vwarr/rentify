6c0b660d7853a2779622c0f1e5ad851132f2f2e1
Varun Warrier <varunswarrier@gmail.com>
2024-08-07 11:36:23 -0400

    deleted unnecessary line

--- rentify/urls.py
+++ rentify/urls.py
@@ -18,11 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-app_name = 'rentify'
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls'), name='accounts'),
     path('items/', include('rentalapp.urls'), name='items')
 ]
