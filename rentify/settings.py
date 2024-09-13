540d76aacaa9730f66010360066eab786228f6cf
korinagoldsmith <korinagoldsmith@gmail.com>
2024-09-13 11:09:41 -0400

    added profile, about, and contact pages

--- rentify/settings.py
+++ rentify/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rentify',
     'items',
-    'accounts'
+    'accounts',
+    'about',
+    'user_profile',
+    'contact'
 ]
 
 MIDDLEWARE = [
