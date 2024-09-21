5d53ce05ade2f715a510eea7eb7decf032faa1c1
korinagoldsmith <korinagoldsmith@gmail.com>
2024-09-21 12:21:11 -0400

    profile page

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
